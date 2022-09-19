000a85dd79a970157dc7a4e436d3a7bf262e5c5e
Mayank Narula <mnarulasubs@gmail.com>
2022-09-19 23:36:56 +0530

    Time: 248 ms (20.23%), Space: 44.6 MB (32.06%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,28 +1,34 @@
-class Solution {
-public:
-    vector<vector<string>> findDuplicate(vector<string>& paths) {
-        vector<vector<string>> dups;
-        unordered_map<string, vector<string>> same;
-        
-        for(string path: paths){
-            
-            stringstream stream(path);
-            string s, dir;
-            getline(stream, dir, ' ');
-            while(getline(stream, s, ' ')){
-                int cs = s.find('('), ce = s.find(')'); 
-                string loc = dir + '/' + s.substr(0, cs);
-                string content = s.substr(cs + 1, ce - cs - 1);
-                same[content].push_back(loc);
+class Solution
+{
+    public:
+        vector<vector < string>> findDuplicate(vector<string> &paths)
+        {
+            vector<vector < string>> dups;
+            unordered_map<string, vector < string>> same;
+
+            for (string path: paths)
+            {
+
+                stringstream stream(path);
+                string s, dir;
+                getline(stream, dir, ' ');
+                while (getline(stream, s, ' '))
+                {
+                    int cs = s.find('('), ce = s.find(')');
+                    string loc = dir + '/' + s.substr(0, cs);
+                    string content = s.substr(cs + 1, ce - cs - 1);
+                    same[content].push_back(loc);
+                }
             }
-        }
-        
-        for(auto [content, files]: same){
-            if(files.size() > 1){
-                dups.push_back(files);
+
+            for (auto[content, files]: same)
+            {
+                if (files.size() > 1)
+                {
+                    dups.push_back(files);
+                }
             }
+
+            return dups;
         }
-        
-        return dups;
-    }
 };
