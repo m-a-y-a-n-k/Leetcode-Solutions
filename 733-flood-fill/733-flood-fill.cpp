eb298c4f6c762dddadfa4f570ba8bbbf8c6c4925
Mayank Narula <mnarulasubs@gmail.com>
2022-09-21 17:56:10 +0530

    Time: 9 ms (86.66%), Space: 14.3 MB (15.94%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,18 +1,16 @@
 class Solution {
     int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    bool safe(int r,int c, int n, int m){
-        return r >= 0 && r < n && c >= 0 && c < m;
-    }
     
     void fill(vector<vector<int>>& image, int r, int c, int n, int m, int s, int t, vector<vector<int>> &F){
-        if(safe(r,c,n,m) && F[r][c] == -1 && image[r][c] == s){
-            image[r][c] = t;
-            F[r][c] = 1;
-            for(int i = 0; i < 4; i++){
-                int x = r + dir[i][0], y = c + dir[i][1];
-                fill(image, x, y, n, m, s, t, F);
-            }
+        if(r < 0 || c < 0 || r >= n || c >= m || image[r][c] != s || F[r][c] != -1){
+            return ;
+        }
+        image[r][c] = t;
+        F[r][c] = 1;
+        for(int i = 0; i < 4; i++){
+            int x = r + dir[i][0], y = c + dir[i][1];
+            fill(image, x, y, n, m, s, t, F);
         }
     }
 public:
