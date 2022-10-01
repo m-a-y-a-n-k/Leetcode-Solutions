7ad52862b8462dd001fbf5cd0489ad99f9099468
Mayank N <mnarulasubs@gmail.com>
2022-10-01 19:05:37 +0530

    Time: 139 ms (38.76%), Space: 21.4 MB (45.66%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,54 +1,79 @@
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 
-class Solution {
-    int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-    bool safe(const pi &K, int n, int m){
-        return (K.first >= 0 && K.first < n && K.second >= 0 && K.second < m);
+class Solution
+{
+    pi dirs[4] = {
+		{ -1, 0 },
+        { 1,
+            0 },
+        { 0,
+            -1 },
+        { 0,
+            1 }
+    };
+
+    bool safe(int x, int y, int n, int m)
+    {
+        return (x >= 0 && x < n && y >= 0 && y < m);
     }
-public:
-    int maxDistance(vector<vector<int>>& grid) {
-        int d = 0, md = -1;
-        int n = size(grid), m = size(grid[0]);
-        queue<pi> Q;
-        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
-        
-        for(int r = 0; r < n; ++r){
-            for(int c = 0; c < m; ++c){
-                if(grid[r][c]){
-                    Q.push({r, c});
-                }
-            }
-        }                
-        
-        while(!Q.empty()){
-            int s = Q.size();
-            d++;
-            
-            for(int x = 0; x < s; ++x){
-                pi P = Q.front();
-                Q.pop();
-                for(int k = 0; k < 4; ++k){
-                    pi K = {P.first + dir[k][0], P.second + dir[k][1]};
-                    if(
-                        safe(K, n, m) && 
-                        !grid[K.first][K.second] && 
-                        dp[K.first][K.second] > d
-                    ){
-                        Q.push({K.first, K.second});
-                        dp[K.first][K.second] = d;
+    public:
+        int maxDistance(vector<vector < int>> &grid)
+        {
+            int d = 0, md = -1;
+            int n = size(grid), m = size(grid[0]);
+            queue<pi> Q;
+            vector<vector < int>> dp(n, vector<int> (m, INT_MAX));
+
+            for (int r = 0; r < n; ++r)
+            {
+                for (int c = 0; c < m; ++c)
+                {
+                    if (grid[r][c])
+                    {
+                        Q.push({ r,
+                            c });
                     }
                 }
             }
-        }
-        
-        for(int r = 0; r < n; ++r){
-            for(int c = 0; c < m; ++c){
-                if(!grid[r][c]){
-                    md = max(md, dp[r][c]);
+
+            while (!Q.empty())
+            {
+                int s = Q.size();
+                d++;
+
+                for (int x = 0; x < s; ++x)
+                {
+                    pi P = Q.front();
+                    Q.pop();
+                    for (auto &dir: dirs)
+                    {
+                        pi K = { P.first + dir.first,
+                            P.second + dir.second
+                        };
+                        if (safe(K.first, K.second, n, m) &&
+                            !grid[K.first][K.second] &&
+                            dp[K.first][K.second] > d
+                       )
+                        {
+                            Q.push({ K.first,
+                                K.second });
+                            dp[K.first][K.second] = d;
+                        }
+                    }
                 }
             }
+
+            for (int r = 0; r < n; ++r)
+            {
+                for (int c = 0; c < m; ++c)
+                {
+                    if (!grid[r][c])
+                    {
+                        md = max(md, dp[r][c]);
+                    }
+                }
+            }
+
+            return md == INT_MAX ? -1 : md;
         }
-        
-        return md == INT_MAX ? -1 : md;
-    }
 };
