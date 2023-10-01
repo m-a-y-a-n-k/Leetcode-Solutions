7c7dd0193e36c6cc94c1a51ce0c6b4f4da8e4832
Mayank N <mnarulasubs@gmail.com>
2023-10-01 20:50:17 +0530

    Added solution - LeetHub

---
+++ Check for Binary - GFG/check-for-binary.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// C++ program to Print root to leaf path WITHOUT
+// using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBinary(string str);
+
+// Driver program to test above functions
+int main()
+{
+    string str;
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
+    {
+       cin >> str;
+       cout << isBinary(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+// Return true if str is binary, else false
+bool isBinary(string str)
+{
+   // Your code here
+   int n = str.size();
+   if(n == 0){
+       return true;
+   }
+   return (str[0] == '1' || str[0] == '0') && isBinary(str.substr(1));
+}
