aab8441d5ad05a69f1d703286557a6ee85856603
Mayank N <mnarulasubs@gmail.com>
2023-10-02 15:13:19 +0530

    Added solution - LeetHub

---
+++ if-else (Decision Making) - GFG/ifelse-decision-making.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        // code here
+        if(n < m){
+            return "lesser";
+        }
+        if(n == m){
+            return "equal";
+        }
+        return "greater";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m ; cin>>n>>m;
+        Solution obj;
+        cout<<obj.compareNM(n,m)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
