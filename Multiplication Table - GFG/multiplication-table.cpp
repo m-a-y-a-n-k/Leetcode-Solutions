f60f00c395a5f590415534b7a3b8b924b30fcf46
Mayank N <mnarulasubs@gmail.com>
2023-10-08 10:16:05 +0530

    Added solution - LeetHub

---
+++ Multiplication Table - GFG/multiplication-table.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> getTable(int N)
+    {
+        // Write Your Code here
+        vector<int> result;
+        
+        for(int m = 1; m <= 10; m++){
+            result.push_back(m * N);
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> ans = ob.getTable(N);
+        for(int i=0; i<ans.size(); i++)
+        cout<<ans[i]<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
